221e52c164f573a3186654765e391e0431b75763
olavodalberto <olavodalberto921@gmail.com>
2025-01-10 12:30:35 -0300

    add rounded format to about

--- src/components/About.js
+++ src/components/About.js
@@ -5,50 +5,37 @@ export default function About() {
     return (
         <section
             id="about"
-            className="px-10 w-full flex flex-col lg:flex-row py-20 
+            className="px-10 w-80 h-80 flex flex-col lg:flex-row py-20 
                        align-center bg-gradient-to-r from-indigo-200 to-indigo-800 
-                       max-w-5xl mx-auto shadow-lg rounded overflow-hidden">
+                       mx-auto shadow-lg rounded-full overflow-hidden">
             <div className="flex-1">
                 <img src={Image}
                     alt="About"
-                    className="w-full h-full object-cover rounded shadow-md"/>
+                    className="w-full h-full object-cover rounded-full shadow-md"/>
             </div>
             <div className="flex-1 flex flex-col justify-center
                             items-center gap-5 px-6">
                 <div>
                     <h2 className="text-center text-blue-800 
-                                   text-5xl font-extrabold mb-6">
+                                   text-2xl font-extrabold mb-2">
                         Sobre mim
                     </h2>
                 </div>
-                <p className="text-center text-lg text-gray-100 leading-relaxed mb-4">
+                <p className="text-center text-sm text-gray-100 leading-relaxed mb-2">
                     Olá, eu sou Olavo Dalberto, um desenvolvedor
-                    apaixonado e autodidata (o que é algo bastante
-                    subjetivo, já que aprendi com professores no
-                    YouTube, então isso realmente conta? Não sei).
-                    Tenho cerca de 2 anos de experiência em 
-                    transformar café e linhas de código em produtos
-                    web funcionais (ou quase sempre funcionais,
-                    depende do bug).
+                    apaixonado e autodidata. Tenho cerca de 2 anos 
+                    de experiência em transformar café e linhas de código 
+                    em produtos web funcionais.
                 </p>
-                <p className="text-center text-lg text-gray-100 leading-relaxed mb-4">
+                <p className="text-center text-sm text-gray-100 leading-relaxed mb-2">
                 Atualmente, sou estudante do 7º semestre de
-                Engenharia de Computação na Universidade Federal
-                de Santa Maria (UFSM). Minha jornada tem sido um 
-                misto de noites longas, aprendizado constante e 
-                aquela luta eterna para lembrar de colocar ponto 
-                e vírgula no final das linhas (quem nunca, né?).
+                Engenharia de Computação na UFSM. Minha jornada é um
+                misto de aprendizado constante e uma luta eterna com
+                ponto e vírgula.
                 </p>
-                <p className="text-center text-lg text-gray-100 leading-relaxed mb-4">
-                Trabalhar em projetos incríveis ao longo desse caminho me permitiu
-                evoluir como desenvolvedor, especialmente na área de desenvolvimento
-                full-stack. Estou sempre em busca de novos desafios, aprendizados e
-                memes relacionados à programação.
-                </p>
-                <p className="text-center text-lg text-gray-100 leading-relaxed">
+                <p className="text-center text-sm text-gray-100 leading-relaxed">
                 Se você acha que posso ajudar no seu próximo projeto, me chama!
-                Prometo trazer muita dedicação, algumas piadas ruins e, com sorte,
-                uma solução para o seu problema.
+                Prometo dedicação, piadas ruins e uma solução para o seu problema.
                 </p>
             </div>
         </section>
