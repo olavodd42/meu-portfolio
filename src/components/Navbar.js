b35c9e2504481be1fbaaf400f022d9b7ae31f602
olavodalberto <olavodalberto921@gmail.com>
2025-02-05 15:52:23 -0300

    melhora design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,21 +10,22 @@ export default function Navbar() {
 
     return (
         <nav className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-black bg-opacity-90 px-6 py-3 rounded-full shadow-lg w-[95%] max-w-md md:px-10 lg:w-[80%] xl:max-w-lg">
-            <ul className="flex sm:flex-wrap md:flex-nowrap justify-around md:justify-center space-x-4 md:space-x-8 text-white text-sm font-semibold tracking-wide">
+            <ul className="flex flex-wrap md:flex-nowrap justify-around md:justify-center space-x-4 md:space-x-8 text-white text-sm font-semibold tracking-wide">
                 {["home", "about", "education", "projects", "contact"].map((item) => (
                     <li key={item} className="relative">
-                        <a
-                            href={`#${item}`}
-                            onClick={() => handleScroll(item)}
-                            className={`${
-                                active === item ? "text-teal-400" : "text-gray-400"
-                            } hover:text-teal-300 transition relative`}
-                        >
-                            {item.toUpperCase()}
-                        </a>
+                    <a
+                        href={`#${item}`}
+                        onClick={() => handleScroll(item)}
+                        className={`${
+                        active === item ? "text-teal-400" : "text-gray-400"
+                        } hover:text-teal-300 transition relative`}
+                    >
+                        {item.toUpperCase()}
+                    </a>
                     </li>
                 ))}
-            </ul>
+                </ul>
+
         </nav>
     );
 }
