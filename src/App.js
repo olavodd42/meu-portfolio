8b2d4f1d0b2e3d49675dcb74e4c166201d82e143
olavodalberto <olavodalberto921@gmail.com>
2025-02-09 21:31:44 -0300

    transicao suave

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Loading3D from './components/Loading3D';
 import Navbar from './components/Navbar';
 import About from './components/About';
@@ -10,32 +11,43 @@ import Hero from './components/Hero';
 import Education from './components/Education';
 import './index.css';
 
-console.log('Tipo de Loading3D:', typeof Loading3D);
-
-
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Simula operações assíncronas (por exemplo, carregamento de dados)
-    const timer = setTimeout(() => setIsLoading(false), 20000); // Aumente o tempo para 3000ms
+    const timer = setTimeout(() => setIsLoading(false), 20000);
     return () => clearTimeout(timer);
   }, []);
 
-  if (isLoading) {
-    return <Loading3D />;
-  }
-
   return (
-    <div className="min-h-screen">
-      <Hero />
-      <Navbar />
-      <About />
-      <Education />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
+    <AnimatePresence>
+      {isLoading ? (
+        <motion.div
+          key="loading"
+          initial={{ opacity: 1 }}
+          exit={{ opacity: 0, transition: { duration: 1 } }}
+        >
+          <Loading3D />
+        </motion.div>
+      ) : (
+        <motion.div
+          key="content"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+        >
+          <div className="min-h-screen">
+            <Hero />
+            <Navbar />
+            <About />
+            <Education />
+            <Skills />
+            <Projects />
+            <Contact />
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
